82cdf69bf36f005947888fd0c016f7524484d501
bitc0m <39449508+bitc0m@users.noreply.github.com>
2018-07-13 08:37:25 -0400

    Create install.sh
    MacOS build script commit.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#download Gitlab Repo for betanet
+curl -o /usr/local/bin/opam -L https://github.com/ocaml/opam/releases/download/2.0.0-rc3/opam-2.0.0-rc3-x86_64-darwin
+
+#assign user rights to /usr/local/bin
+sudo chown -R $(whoami) /usr/local/bin
+
+#make opam executable
+sudo chmod a+x /usr/local/bin/opam
+
+#initiate Opam
+opam init -y --compiler=4.06.1
+
+#evalute configuration environment
+eval `opam config env`
+
+
+git clone -b betanet https://gitlab.com/tezos/tezos.git & wait
+{ sleep 5; echo waking up after 5 seconds; } & wait
+cd tezos
+
+#build dependencies
+make build-deps
+
+#evaluate
+eval $(opam env)
+
+#build 
+make
