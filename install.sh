068dae2e80c47585d0ab1726684b44ba8554d006
bitc0m <39449508+bitc0m@users.noreply.github.com>
2018-07-14 16:57:55 -0400

    Update install.sh
    Rewriting to cover quick brew install. Saving the full package build version for a later date it is currently not ready.

--- install.sh
+++ install.sh
@@ -1,96 +1,47 @@
 #!/bin/sh
-
-WORK=$HOME/Builds/build-essential
-PREFIX=$HOME/local
-export PATH="$PREFIX/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+PREFIX=/usr/local/bin
 
 mkdir -p $WORK/src
 
-cd $WORK/src
-curl -O -L ftp://ftp.gnu.org/gnu/m4/m4-latest.tar.xz
-curl -O -L ftp://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.xz
-curl -O -L ftp://ftp.gnu.org/gnu/automake/automake-1.16.tar.xz
-curl -O -L ftp://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz
-curl -O -L https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
-curl -O -L https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
+cd $PREFIX
 curl https://github.com/ocaml/opam/releases/download/2.0.0-rc3/opam-2.0.0-rc3-x86_64-darwin > $PREFIX/opam
 
-cd $WORK
-
 # m4
-tar xf src/m4-*
-cd m4-*
-./configure\
-    --prefix=$PREFIX
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr m4-*
+brew install m4
+brew link m4
 
 # autoconf
-tar xf src/autoconf-2*
-cd autoconf-2*
-./configure\
-    --prefix=$PREFIX\
-    --disable-debug
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr autoconf-2*
+brew install autoconf
+brew link autoconf
 
 # automake
-tar xf src/automake-*
-cd automake-*
-./configure\
-    --prefix=$PREFIX
-    --disable-debug
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr automake-*
+brew install automake
+brew link automake
 
 # libtool
-tar xf src/libtool-*
-cd libtool-*
-./configure\
-    --prefix=$PREFIX
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr libtool-*
+brew install libtool
+brew link libtool
 
 # gmp
-tar xf src/gmp-*
-cd gmp-*
-./configure\
-    --prefix=$PREFIX
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr gmp-*
+brew install gmp
+brew link gmp
 
 # pkg-conf
-tar zxvf src/pkg-config-*
-cd pkg-config-*
-./configure --with-internal-glib\
-    --prefix=$PREFIX
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr pkg-config-*
+brew install pkg-config
+brew link pkg-config
 
 # hidapi
-unzip src/hidapi-*
-cd hidapi-*\mac
-./configure \
-    --prefix=$PREFIX
-make clean && make -j4 && make install-strip
-cd $WORK
-rm -fr hidapi-*
+brew install hidapi
+brew link hidapi
 
 #make opam executable
 sudo chmod a+x $PREFIX/opam
 
 #initiate Opam
-opam init -y --compiler=4.06.1
+$PREFIX/opam init -y --compiler=4.06.1
 
 #evalute configuration environment
 eval `opam config env`
-
 
 git clone -b betanet https://gitlab.com/tezos/tezos.git & wait
 { sleep 5; echo waking up after 5 seconds; } & wait
