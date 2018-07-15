17d51adec1bf9fa1c8bca2b073c988635bbd7c3e
bitc0m <39449508+bitc0m@users.noreply.github.com>
2018-07-14 23:21:52 -0400

    Update monit-install.sh

--- monit-install.sh
+++ monit-install.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 PREFIX=/usr/local/bin
-sudo chown -R $(whoami):admin $PREFIX
+WORK=$HOME/src
+mkdir -p $WORK
 
-#curl -o $PREFIX/opam -L https://github.com/ocaml/opam/releases/download/2.0.0-rc3/opam-2.0.0-rc3-x86_64-darwin
 curl -O -L https://mmonit.com/monit/dist/monit-5.25.2.tar.gz
 
 # monit
-tar xf src/monit-*
+tar xf monit-*
 cd monit-*
-./configure\
+./configure --without-ssl\
     --prefix=$PREFIX
 make clean && make -j4 && make install-strip
 cd $WORK
 rm -fr monit-*
+cd $HOME
+rm -rf $WORK
