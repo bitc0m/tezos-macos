69613b7a215a23e36eeaa4fc9a24c3f831c2f045
Shaun Belcher <39449508+bitc0m@users.noreply.github.com>
2018-08-05 21:17:47 -0400

    Create be.sh

---
+++ be.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# m4
+brew install m4
+brew link m4
+
+# autoconf
+brew install autoconf
+brew link autoconf
+
+# automake
+brew install automake
+brew link automake
+
+# libtool
+brew install libtool
+brew link libtool
+
+# gmp
+brew install gmp
+brew link gmp
+
+# pkg-conf
+brew install pkg-config
+brew link pkg-config
+
+# hidapi
+brew install hidapi
+brew link hidapi
